88a617b853db60b40438b587ea8cbda28709a96a
Arsema-Seife <arsemaseife137@gmail.com>
2025-12-02 06:34:30 -0800

    Rename the Dart file names

---
+++ Month 1/Week 1/Task 2-Fizz Buzz.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  print('Enter a number:');
+  String input = stdin.readLineSync()!; 
+  int number = int.parse(input);        
+
+  if ((number % 3 == 0) && (number % 5 == 0)) {
+    print('FizzBuzz');
+  } else if (number % 3 == 0) {
+    print('Fizz');
+  } else if (number % 5 == 0) {
+    print('Buzz');
+  } else {
+    print('Not divisible by neither');
+  }
+}
+  
